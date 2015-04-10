b303f76c954730572d6bc457b3eda80f867cfad5
sivaljunior <jr_sival@hotmail.com>
2015-04-10 09:23:00 -0300

    Métodos de Calcula

--- Carrinho.java
+++ Carrinho.java
@@ -1,5 +1,20 @@
 class Carrinho{
 	ArrayList<Produto> prod = new ArrayList<Produto>;
-	
+
+	public double calculaPeso(Produto P){
+		double peso = 0.0;
+		for(int i = 0; i < prod.length; i++) peso += prod[i].getPeso;
+		return peso;
+	}
+
+	public double calculaFrete(double a, double b){
+		return a * calculaPeso();
+	}	
+
+	public double calculaPreço(Produto P){
+		double preço = 0.0;
+		for(int i = 0; i < prod.length; i++) preço += prod[i].getPreço;
+		return preço;
+	}	
 	
 }
