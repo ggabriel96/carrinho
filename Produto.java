bdbda7747502e79b24430fc93fe295ec3aa6bf71
Gabriel Galli <g7.galli96@gmail.com>
2015-04-10 09:16:08 -0300

    Feitos todos os getters e setters

--- Produto.java
+++ Produto.java
@@ -7,11 +7,23 @@ class Produto {
 		return this.Peso;
 	}
 	
+	public void setPeso(double p) {
+		this.Peso = p;
+	}
+	
 	public double getPreço() {
 		return this.Preço;
 	}
 	
+	public void setPreço(double p) {
+		this.Preço = p;
+	}
+	
 	public String getNome() {
 		return this.nome;
+	}
+	
+	public void setNome(String n)  {
+		this.nome = n;
 	}
 }
