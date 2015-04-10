420a4e98bfaec20862120739094643c9cf46c500
Gabriel Galli <g7.galli96@gmail.com>
2015-04-10 09:12:33 -0300

    Criada classe Produto

---
+++ Produto.java
@@ -0,0 +1,17 @@
+class Produto {
+	private double Peso;
+	private double Preço;
+	private String nome;
+	
+	public double getPeso() {
+		return this.Peso;
+	}
+	
+	public double getPreço() {
+		return this.Preço;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+}
