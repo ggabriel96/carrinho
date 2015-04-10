b91231226b41d0a9b39b642c6abd48a34069127f
Eu <aaa@hota.com>
2015-04-10 09:20:27 -0300

    Metodos adicionados

--- Carrinho.java
+++ Carrinho.java
@@ -1,5 +1,23 @@
 class Carrinho{
-	ArrayList<Produto> prod = new ArrayList<Produto>;
+	ArrayList<Produto> prod = new ArrayList<Produto>();
+	
+	public Produto adicionaProduto(Produto p){
+		prod.add(p);
+		return p;
+	}
+	
+	public void removeProduto(Produto p){
+		prod.remove(Produto p);
+	}
+	
+	public double preçototal(double a, double b){
+		double resp = 0;
+		for(i = 0; i < prod.size(); i++){
+			resp += prod.get(i).getPreço();
+		}
+		return resp;
+	}
+	
 	
 	
 }
